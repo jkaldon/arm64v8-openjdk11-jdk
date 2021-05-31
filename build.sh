17c78a455b5dd6f457c40323e86aa6846ec914b3
Joshua Kaldon <jkaldon@lenistech.com>
2021-05-30 23:32:36 -0500

    updating tag format to include jdk and alpine version info

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -x
 
 docker run --rm --privileged linuxkit/binfmt:v0.8
-docker buildx build --progress plain --no-cache --pull --platform linux/arm64 -t jkaldon/arm64v8-openjdk:11-jdk-alpine --push .
+docker buildx build --progress plain --no-cache --pull --platform linux/arm64 -t jkaldon/arm64v8-openjdk:jdk11-alpine3.13 --push .
